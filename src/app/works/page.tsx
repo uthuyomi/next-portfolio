d93258927dadc548911801f9f20a2485f7ae5c20
uthuyomi <kaiseif4e@gmail.com>
2025-09-30 02:48:38 +0900

    auto-commit

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-import { Swiper , SwiperSlide} from 'swiper/react';
-import 'swiper/css';
-import React from 'react'
+import React from 'react';
 import Hero from "@/components/Hero";
 
 export default function Home() {
@@ -9,10 +6,7 @@ export default function Home() {
     <main>
       <Hero />
       <div className="bg-[#1e293b] py-12 flex flex-col items-center">
-        <Swiper className="w-sm h-80 bg-amber-50 ">
-          <SwiperSlide></SwiperSlide>
-          <SwiperSlide></SwiperSlide>
-        </Swiper>
+        
       </div>
     </main>
   );
--- src/components/works/slide.tsx
+++ src/components/works/slide.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import Image from "next/image";
+import React from "react";
 
-const slide = () => {
-    return (
-        
-  )
+type slideProps = {
+  slide: {
+    pcimg: string[]
+    spimg: string[]
+  }
 }
 
-export default slide
+const slide = ({data}:slideProps) => {
+  return (
+    <Swiper className="w-sm h-80 bg-amber-50 ">
+      <SwiperSlide className="bg-[url('/images/works/pc-back.png')] bg-cover bg-center">
+        <Image src={data.pcimg} alt="pc画像" width={100} height={100} />
+      </SwiperSlide>
+      <SwiperSlide className="bg-[url('/images/works/sp-back.png')] bg-cover bg-center">
+        <Image src={data.spimg} alt="pc画像" width={100} height={100} />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default slide;
