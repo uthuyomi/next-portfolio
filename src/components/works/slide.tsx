f009c5c9911683317df1a30c3aa5364d1f34aaca
uthuyomi <kaiseif4e@gmail.com>
2025-10-01 02:23:39 +0900

    auto-commit

--- src/components/works/slide.tsx
+++ src/components/works/slide.tsx
@@ -6,20 +6,17 @@ import React from "react";
 
 type slideProps = {
   slide: {
-    pcimg: string[]
-    spimg: string[]
-  }
-}
+    pcimg: string[];
+    spimg: string[];
+  };
+};
 
-const slide = ({slide}:slideProps) => {
+const slide = ({ slide }: slideProps) => {
   return (
     <Swiper className="w-sm h-80 bg-amber-50 ">
-      {slide.map((item, i) => (
-        <SwiperSlide key={i} className="bg-[url('/images/works/pc-back.png')] bg-cover bg-center">
-          <Image src={item.pcimg} alt="pc画像" width={100} height={100} />
-        </SwiperSlide>
-      ))}
-      {slide.map((item, i) => (
+      <SwiperSlide className="bg-[url('/images/works/pc-back.png')] bg-cover bg-center">
+        <Image src={slide.pcimg} alt="pc画像" width={100} height={100} />
+      </SwiperSlide>
       <SwiperSlide className="bg-[url('/images/works/sp-back.png')] bg-cover bg-center">
         <Image src={item.spimg} alt="pc画像" width={100} height={100} />
       </SwiperSlide>
