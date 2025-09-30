ac40bf56edbec7d7af70dd9dbb601fe5acd7df0d
uthuyomi <kaiseif4e@gmail.com>
2025-10-01 01:04:33 +0900

    auto-commit

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from "@/components/Hero";
+import Slide from "@/components/works/slide";
+import data from "@/data/worksdata.json";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <div className="bg-[#1e293b] py-12 flex flex-col items-center">
-        
+        <Slide data={data.slide01} />
       </div>
     </main>
   );
--- src/components/works/slide.tsx
+++ src/components/works/slide.tsx
@@ -11,14 +11,17 @@ type slideProps = {
   }
 }
 
-const slide = ({data}:slideProps) => {
+const slide = ({slide}:slideProps) => {
   return (
     <Swiper className="w-sm h-80 bg-amber-50 ">
-      <SwiperSlide className="bg-[url('/images/works/pc-back.png')] bg-cover bg-center">
-        <Image src={data.pcimg} alt="pc画像" width={100} height={100} />
-      </SwiperSlide>
+      {slide.map((item, i) => (
+        <SwiperSlide key={i} className="bg-[url('/images/works/pc-back.png')] bg-cover bg-center">
+          <Image src={item.pcimg} alt="pc画像" width={100} height={100} />
+        </SwiperSlide>
+      ))}
+      {slide.map((item, i) => (
       <SwiperSlide className="bg-[url('/images/works/sp-back.png')] bg-cover bg-center">
-        <Image src={data.spimg} alt="pc画像" width={100} height={100} />
+        <Image src={item.spimg} alt="pc画像" width={100} height={100} />
       </SwiperSlide>
     </Swiper>
   );
