77d382e7a7caed618dd3a2ea4e7782a087dea642
uthuyomi <kaiseif4e@gmail.com>
2025-10-01 06:25:39 +0900

    auto-commit

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React from "react";
 import Hero from "@/components/Hero";
-import Slide from "@/components/works/slide";
+import Slide from "@/components/works/Slide";
 import data from "@/data/worksdata.json";
 
 export default function Home() {
+  const base = data;
   return (
     <main>
       <Hero />
       <div className="bg-[#1e293b] py-12 flex flex-col items-center">
-        <Slide data={data.slide01} />
+        <Slide data={base.slide01} />
+        <Slide data={base.slide02} />
       </div>
     </main>
   );
--- src/components/works/slide.tsx
+++ src/components/works/slide.tsx
@@ -1,27 +1,48 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
+import style from "./slide.module.scss";
 import Image from "next/image";
 import React from "react";
+import pcOuter from "../../../public/images/works/pc-back.png";
+import spOuter from "../../../public/images/works/sp-back.png";
 
 type slideProps = {
-  slide: {
-    pcimg: string[];
-    spimg: string[];
+  data: {
+    pcimg: string;
+    spimg: string;
   };
 };
 
-const slide = ({ slide }: slideProps) => {
+const Slide = ({ data }: slideProps) => {
   return (
-    <Swiper className="w-sm h-80 bg-amber-50 ">
-      <SwiperSlide className="bg-[url('/images/works/pc-back.png')] bg-cover bg-center">
-        <Image src={slide.pcimg} alt="pc画像" width={100} height={100} />
+    <Swiper className="w-sm aspect-[1920/1280] bg-amber-50 ">
+      <SwiperSlide className={`${style["slide-pc"]} bg-cover bg-center`}>
+        <Image
+          className={style.slideOuter}
+          src={pcOuter}
+          alt="背景画像"
+          width={100}
+          height={100}
+        />
+        <div className={style.slideInner}>
+          <Image src={data.pcimg} alt="pc画像" width={1920} height={1280} unoptimized/>
+        </div>
       </SwiperSlide>
-      <SwiperSlide className="bg-[url('/images/works/sp-back.png')] bg-cover bg-center">
-        <Image src={item.spimg} alt="pc画像" width={100} height={100} />
+      <SwiperSlide className={`${style["slide-sp"]} bg-cover bg-center`}>
+        <Image
+          className={style.slideOuter}
+          src={spOuter}
+          alt="背景画像"
+          width={100}
+          height={100}
+        />
+        <div className={style.slideInner}>
+          <Image src={data.spimg} alt="pc画像" width={1920} height={1280} unoptimized/>
+        </div>
       </SwiperSlide>
     </Swiper>
   );
 };
 
-export default slide;
+export default Slide;
